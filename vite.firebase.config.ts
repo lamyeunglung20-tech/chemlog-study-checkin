5ba70c3ba32c63e0d544039a91baa5141929b357
marcolam <lamyeunglung20@gmail.com>
2026-09-13 16:49:25 +0800

    Fix static asset delivery

--- vite.firebase.config.ts
+++ vite.firebase.config.ts
@@ -8,5 +8,15 @@ export default defineConfig({
   build: {
     outDir: '../firebase-dist',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/scheduler')) return 'react-vendor';
+          if (id.includes('@firebase/auth')) return 'firebase-auth';
+          if (id.includes('@firebase/firestore')) return 'firebase-firestore';
+          if (id.includes('@firebase/')) return 'firebase-core';
+        },
+      },
+    },
   },
 });
